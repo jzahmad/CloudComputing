0f71be79e0f45b9db99875093387e129a8545e82
Jazib Ahmad <jz346475@dal.ca>
2024-05-10 01:36:13 -0300

    done

---
+++ A3/A3Bycrypt/index.mjs
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import Bycrypt from 'bycrypt'
+
+export const handler = async (event) => {
+    try {
+        const action = event.action;
+        const value = event.value;
+        const courseUri = event.course_uri;
+        
+        // Hash the value using bcrypt
+        const hashedValue = await bcrypt.hash(password, 10);
+        
+        // Prepare the data to send in the POST request
+        const postData = {
+            banner: "B00889804",
+            result: hashedValue,
+            arn: "arn:aws:lambda:us-east-1:767397661521:function:A3Bycrypt",
+            action: action,
+            value: value
+        };
+
+        // Make an HTTP POST request to the course_uri
+        const response = await axios.post(courseUri, postData);
+
+        // Return the response from the POST request
+        return {
+            statusCode: response.status,
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Internal Server Error'
+            })
+        };
+    }
+};
---
+++ A3/A3sha256/index.mjs
@@ -0,0 +1,39 @@
+import crypto from 'crypto';
+import axios from 'axios';
+
+export const handler = async (event) => {
+    try {
+        const action = event.action;
+        const value = event.value;
+        const courseUri = event.course_uri;
+        
+        // Hash the value using MD5
+        const hashedValue = crypto.createHash('sha256').update(value).digest('hex');
+        
+        // Prepare the data to send in the POST request
+        const postData = {
+            banner: "B00889804",
+            result: hashedValue,
+            arn: "arn:aws:lambda:us-east-1:767397661521:function:A3sha256",
+            action: action,
+            value: value
+        };
+
+        // Make an HTTP POST request to the course_uri
+        const response = await axios.post(courseUri, postData);
+
+        // Return the response from the POST request
+        return {
+            statusCode: response.status,
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Internal Server Error'
+            })
+        };
+    }
+};
